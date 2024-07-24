2af0d5c8c9b36cf53bbc7292cc19479f36ae37df
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-24 06:03:34 +0100

    update task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,19 @@
-const { createServer } = require('node:http');
+/**
+ * Simple HTTP server
+ */
+const http = require('http');
 
-const hostname = '127.0.0.1';
+const host = '127.0.0.1';
 const port = 1245;
-
-const app = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+const app = http.createServer((req, resp) => {
+  // eslint-disable-next-line no-param-reassign
+  resp.statusCode = 200;
+  resp.setHeader('Content-Type', 'text/plain');
+  resp.end('Hello Holberton School!');
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, host, () => {
+  console.log(`Server is live, running at http://${host}:${port}`);
 });
+
 module.exports = app;
