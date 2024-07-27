2273ce4aed92718aa89195a661e7176c774d0515
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-27 05:41:14 +0100

    update

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,9 +1,22 @@
-const Utils = {
-    calculateNumber: function(type, a, b) {
-      if (type === 'SUM') {
-        return Math.round(a) + Math.round(b);
-      }
-    }
-};
+#!/usr/bin/env node
 
-module.exports = Utils;
+class Utils {
+    /**
+     * calculateNumber - accepts two arguments (number) a and b
+     * @type can be SUM, SUBTRACT, or DIVIDE (string)
+     * @a being the first arguement
+     * @b being the second arguement
+     */
+  
+    static calculateNumber(type, a, b) {
+      const operands = {
+        SUM: (a, b) => a + b,
+        SUBTRACT: (a, b) => a - b,
+        DIVIDE: (a, b) => (b !== 0 ? a / b : 'Error'),
+      };
+  
+      return operands[type](Math.round(a), Math.round(b));
+    }
+  }
+  
+  module.exports = Utils;
