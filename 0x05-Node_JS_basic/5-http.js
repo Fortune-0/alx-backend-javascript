8ead3709f31a696534105de1f97fb71dcf4f3c1a
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-27 01:50:55 +0100

    t@sk5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -9,42 +9,45 @@ const db = process.argv[2] === undefined ? '' : process.argv[2];
 const host = '127.0.0.1';
 const port = 1245;
 const app = http.createServer((req, resp) => {
-    if (req.url === '/') {
-        resp.statusCode = 200;
-        resp.setHeader('Content-Type', 'text/plain');
-        resp.end('Hello Holberton School!');
-    } else if (req.url === '/students') {
-        resp.statusCode = 200;
-        resp.setHeader('Content-Type', 'text/plain');
-        fs.readFile(db, 'utf8', (err, data) => {
-            if (err) {
-                resp.end('Cannot load the database');
-                return;
-            }
-            const students = data.split('\n').filter(line => line.trim() !== '');
-            const csStudents = [];
-            const sweStudents = [];
-            for (const student of students) {
-                const [firstName, lastName, age, field] = student.split(',');
-                if (field === 'CS') {
-                    csStudents.push(firstName);
-                } else if (field === 'SWE') {
-                    sweStudents.push(firstName);
-                }
-            }
-            const response = `This is the list of our students\n` +
-                `Number of students: ${students.length}\n` +
-                `Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n` +
-                `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`;
-            resp.end(response);
-        });
-    } else {
-        resp.statusCode = 404;
-        resp.setHeader('Content-Type', 'text/plain');
-        resp.end('Not Found');
-    }
-});
+  // eslint-disable-next-line no-param-reassign
+  resp.statusCode = 200;
+  resp.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    resp.end('Hello Holberton School!');
+  }
+  if (req.url === '/students') {
+    const body = ['This is the list of our students'];
+    fs.readFile(db, 'utf-8', (error, data) => {
+      if (error) {
+        body.push('Cannot load the database');
+        resp.end(body.join('\n'));
+      } else {
+        const courses = new Map();
+        let students = data.split('\n');
+        students = students.slice(1, students.length - 1);
 
+        // Parse CSV data creating a map of courseData objects.
+        students.forEach((student) => {
+          const studentData = student.split(',');
+          const firstName = studentData[0];
+          const field = studentData[3];
+          if (courses.has(field)) {
+            courses.get(field).push(firstName);
+          } else {
+            courses.set(field, [firstName]);
+          }
+        });
+
+        // Organize data in an array
+        body.push(`Number of students: ${students.length}`);
+        courses.forEach((courseStudents, course) => {
+          body.push(`Number of students in ${course}: ${courseStudents.length}. List: ${courseStudents.join(', ')}`);
+        });
+      }
+      resp.end(body.join('\n'));
+    });
+  }
+});
 
 app.listen(port, host, () => {
   console.log(`Server is live, running at http://${host}:${port}`);
