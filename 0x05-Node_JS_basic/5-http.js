39de06ef056434180a63ff1f002113fc54983583
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-27 02:02:38 +0100

    linter

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,22 +1,19 @@
-/**
- * HTTP sever that handles / and /students
- * routes
- */
 const http = require('http');
 const fs = require('fs');
 
 const db = process.argv[2] === undefined ? '' : process.argv[2];
 const host = '127.0.0.1';
 const port = 1245;
+
 const app = http.createServer((req, resp) => {
-  // eslint-disable-next-line no-param-reassign
   resp.statusCode = 200;
   resp.setHeader('Content-Type', 'text/plain');
+  
   if (req.url === '/') {
     resp.end('Hello Holberton School!');
-  }
-  if (req.url === '/students') {
+  } else if (req.url === '/students') {
     const body = ['This is the list of our students'];
+    
     fs.readFile(db, 'utf-8', (error, data) => {
       if (error) {
         body.push('Cannot load the database');
@@ -24,9 +21,8 @@ const app = http.createServer((req, resp) => {
       } else {
         const courses = new Map();
         let students = data.split('\n');
-        students = students.slice(1, students.length - 1);
+        students = students.slice(1, students.length - 1); // Remove the header and empty lines
 
-        // Parse CSV data creating a map of courseData objects.
         students.forEach((student) => {
           const studentData = student.split(',');
           const firstName = studentData[0];
@@ -38,14 +34,16 @@ const app = http.createServer((req, resp) => {
           }
         });
 
-        // Organize data in an array
         body.push(`Number of students: ${students.length}`);
         courses.forEach((courseStudents, course) => {
           body.push(`Number of students in ${course}: ${courseStudents.length}. List: ${courseStudents.join(', ')}`);
         });
+        resp.end(body.join('\n'));
       }
-      resp.end(body.join('\n'));
     });
+  } else {
+    resp.statusCode = 404;
+    resp.end('Not Found');
   }
 });
 
