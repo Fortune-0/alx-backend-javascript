d555ef8f455a0d0690f9ce06f46a1d78c835f62f
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-27 01:40:08 +0100

    task5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,39 +1,53 @@
+/**
+ * HTTP sever that handles / and /students
+ * routes
+ */
 const http = require('http');
 const fs = require('fs');
 
 const db = process.argv[2] === undefined ? '' : process.argv[2];
 const host = '127.0.0.1';
 const port = 1245;
-
 const app = http.createServer((req, resp) => {
-    resp.statusCode = 200;
-    resp.setHeader('Content-Type', 'text/plain');
-
     if (req.url === '/') {
+        resp.statusCode = 200;
+        resp.setHeader('Content-Type', 'text/plain');
         resp.end('Hello Holberton School!');
     } else if (req.url === '/students') {
-        // Read content from the file 3-read_file_async.js
-        // Construct the appropriate response (you'll need to implement this part)
-        // Example: Read the file, parse data, and display student information
-        fs.readFile(db, 'utf-8', (error, data) => {
-            if (error) {
+        resp.statusCode = 200;
+        resp.setHeader('Content-Type', 'text/plain');
+        fs.readFile(db, 'utf8', (err, data) => {
+            if (err) {
                 resp.end('Cannot load the database');
-            } else {
-                const students = data.split('\n').filter(line => line.trim() !== '').slice(1); // assuming the first line is a header
-                const response = ['This is the list of our students'];
-                response.push(`Number of students: ${students.length}`);
-                students.forEach(student => response.push(student));
-                resp.end(response.join('\n'));
+                return;
             }
+            const students = data.split('\n').filter(line => line.trim() !== '');
+            const csStudents = [];
+            const sweStudents = [];
+            for (const student of students) {
+                const [firstName, lastName, age, field] = student.split(',');
+                if (field === 'CS') {
+                    csStudents.push(firstName);
+                } else if (field === 'SWE') {
+                    sweStudents.push(firstName);
+                }
+            }
+            const response = `This is the list of our students\n` +
+                `Number of students: ${students.length}\n` +
+                `Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n` +
+                `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`;
+            resp.end(response);
         });
     } else {
         resp.statusCode = 404;
+        resp.setHeader('Content-Type', 'text/plain');
         resp.end('Not Found');
     }
 });
 
+
 app.listen(port, host, () => {
-    console.log(`Server is live, running at http://${host}:${port}`);
+  console.log(`Server is live, running at http://${host}:${port}`);
 });
 
 module.exports = app;
