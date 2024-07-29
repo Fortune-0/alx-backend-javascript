da09fcfa23220bebd453baa7c01a65391c8d31b2
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-29 01:35:40 +0100

    Update

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -8,11 +8,12 @@ const port = 7865;
 const app = express();
 
 app.get('/', (req, res) => {
+  res.statusCode = 200;
   res.send('Welcome to the payment system!');
 });
 
 app.listen(port, host, () => {
-  console.log(`Server is live, running at http://${host}:${port}`);
+  console.log(`Server is live, running at ${port}`);
 });
 
 module.exports = app;
