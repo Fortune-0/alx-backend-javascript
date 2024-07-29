f5c5ac073600baafcbbcb11e1b3d03c6c45b5669
fortune-0 <fortunepeterspc07@gmail.com>
2024-07-29 05:26:07 +0100

    upd@te

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -2,13 +2,12 @@
  * Simple express server
  */
 const express = require('express');
-
-const port = 7865;
 const app = express();
+const port = 7865;
 
 app.get('/', function (req, res) {
   res.statusCode = 200;
-  res.send('Welcome to the payment system!');
+  res.send('Welcome to the payment system');
 });
 
 app.listen(port, function () {
